d58686784b83448c143d91db1ad1b08333b4a5af
Sharad Chand <sharad.d.chand@gmail.com>
2018-08-16 16:51:09 +0545

    Additional doc comments for the fields

--- dom_ruukh/src/velement.rs
+++ dom_ruukh/src/velement.rs
@@ -3,13 +3,19 @@ use KeyedVNodes;
 /// The representation of an element in virtual DOM.
 #[derive(Debug)]
 pub struct VElement {
+    /// The tag of the element. Eg: h, p, div, ...
     pub tag: String,
+    /// The attributes of the given element
     pub attributes: Vec<Attribute>,
+    /// The child node of the given element
     pub child: Option<Box<KeyedVNodes>>,
 }
 
+/// The key, value pair of the attributes on an element.
 #[derive(Debug)]
 pub struct Attribute {
+    /// The key of the attribute
     pub key: String,
+    /// The value pair of the attribute key
     pub value: String,
 }
--- dom_ruukh/src/vtext.rs
+++ dom_ruukh/src/vtext.rs
@@ -1,6 +1,8 @@
 /// The representation of string in virtual dom tree.
 #[derive(Debug)]
 pub struct VText {
+    /// The content of a text string
     pub content: String,
+    /// Whether the content is a comment
     pub is_comment: bool
 }
