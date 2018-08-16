93a369ff681561eb1fd93c9d0f9f2d2cc1aedc6b
Sharad Chand <sharad.d.chand@gmail.com>
2018-08-16 15:24:37 +0545

    Added the repr of a list of vnodes

--- dom_ruukh/src/lib.rs
+++ dom_ruukh/src/lib.rs
@@ -1,14 +1,17 @@
 //! The Virtual DOM library which backs the `ruukh` frontend framework.
 
 use velement::VElement;
+use vlist::VList;
 use vtext::VText;
 
-mod vtext;
 mod velement;
+mod vlist;
+mod vtext;
 
 /// A virtual node in a virtual DOM tree.
 #[derive(Debug)]
 pub enum VNode {
     Text(VText),
     Element(VElement),
+    List(VList),
 }
---
+++ dom_ruukh/src/vlist.rs
@@ -0,0 +1,5 @@
+use VNode;
+
+/// The representation of a list of vnodes in the vtree.
+#[derive(Debug)]
+pub struct VList(Vec<VNode>);
