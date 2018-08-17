003980dd0d5b23b095f22c0b8640c8627d3a0b00
Sharad Chand <sharad.d.chand@gmail.com>
2018-08-17 15:37:58 +0545

    Initial design of the component system

---
+++ dom_ruukh/src/component.rs
@@ -0,0 +1,81 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use KeyedVNodes;
+
+/// Trait to define a Component. You do not need to implement this trait.
+/// Use the auto derive provided as `#[derive(Component)]`.
+///
+/// A concrete implementation of a component must have two forms of state.
+/// One, the read only state which is accessed by the user. Another, a
+/// meta-state which stores the mutable state as well as other helpful values
+/// to notify for component re-render.
+///
+/// Note: The user should not worry about the implementation detail of the
+/// actual component. Use the auto derive on the component.
+pub trait Component: Render {
+    type Props;
+    type State: Default;
+
+    /// Initializes a component with props as well as a meta-state called status
+    fn init(props: Self::Props, status: ComponentStatus<Self::State>) -> Self;
+
+    /// Consume props of the component
+    fn props(self) -> Self::Props;
+
+    /// Clone the status of the component
+    fn status(&self) -> ComponentStatus<Self::State>;
+
+    /// Reuse the status of an older component without losing its state
+    ///
+    /// Note: After reusing the status, make sure to invoke `refresh_state()`
+    /// to update the read only state.
+    fn reuse_status(&mut self, status: ComponentStatus<Self::State>);
+
+    /// Update the read only state from the mutated status
+    fn refresh_state(&mut self);
+
+    /// To find whether the component status has been altered
+    fn is_dirty(&mut self) -> bool;
+}
+
+// Stores the metadata related to the state along with the state
+struct Status<T> {
+    pub state: T,
+    pub dirty: bool,
+}
+
+/// Stores the state as well as the metadata to the state
+pub struct ComponentStatus<T>(Rc<RefCell<Status<T>>>);
+
+impl<T> ComponentStatus<T> {
+    pub fn new(state: T) -> ComponentStatus<T> {
+        ComponentStatus(Rc::new(RefCell::new(Status {
+            state,
+            dirty: false,
+        })))
+    }
+}
+
+/// The lifecycle of a stateful component. Implement only the appropriate
+/// hooks as needed.
+///
+/// For stateless component, mark the component with `#[stateless]`.
+pub trait Lifecycle: Component {
+    /// Invoked when the component is first created
+    fn created(&self) {}
+
+    /// Invoked when the component props are updated
+    #[allow(unused_variables)]
+    fn updated(&self, old_props: Self::Props) {}
+
+    /// Invoked when the component is mounted onto the DOM tree
+    fn mounted(&self) {}
+
+    /// Invoked when the component is removed from the DOM tree
+    fn destroyed(&self) {}
+}
+
+/// Trait to render a view for the component.
+pub trait Render {
+    fn render(&self) -> KeyedVNodes;
+}
---
+++ dom_ruukh/src/vcomponent.rs
@@ -0,0 +1,115 @@
+use component::{ComponentStatus, Lifecycle};
+use std::any::Any;
+use std::fmt::{self, Debug, Formatter};
+use KeyedVNodes;
+
+/// The representation of a component in a Virtual DOM.
+pub struct VComponent {
+    /// The manager of the components, handles render as well as state management.
+    manager: Box<ComponentManager>,
+    /// If there are no changes in the state/props of the component, reuse the render
+    cached_render: Option<Box<KeyedVNodes>>,
+}
+
+impl VComponent {
+    pub fn new<T: Lifecycle + Debug + 'static>(props: T::Props) -> VComponent {
+        VComponent {
+            manager: Box::new(ComponentWrapper::<T>::new(props)),
+            cached_render: None,
+        }
+    }
+}
+
+impl Debug for VComponent {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "VComponent {{ component: {}, cached_render: {:?} }}",
+            self.manager.debug(),
+            self.cached_render
+        )
+    }
+}
+
+/// Trait to handle the lifecycle of the component from initialization to render.Box<ComponentManager>
+/// Currently, the interface is not thought out, will define it as needed.
+trait ComponentManager: Downcast {
+    /// The debug implementation of the component.
+    fn debug(&self) -> String;
+
+    /// Try to merge the state of the older component with self
+    fn merge(&mut self, other: Box<ComponentManager>) -> Option<Box<ComponentManager>>;
+
+    /// Generate a markup from the component
+    fn render(&self) -> KeyedVNodes;
+}
+
+type CastInto<T> = fn(Box<ComponentManager>) -> Result<T, Box<ComponentManager>>;
+
+struct ComponentWrapper<T: Lifecycle + 'static> {
+    component: T,
+    try_cast: CastInto<T>,
+}
+
+impl<T: Lifecycle + 'static> ComponentWrapper<T> {
+    fn new(props: T::Props) -> ComponentWrapper<T> {
+        ComponentWrapper {
+            component: T::init(props, ComponentStatus::new(T::State::default())),
+            try_cast: |other| {
+                let mut same_type = false;
+                {
+                    let any = &other as &Any;
+                    if any.is::<T>() {
+                        same_type = true;
+                    }
+                }
+
+                if same_type {
+                    let boxed = other.into_any();
+                    Ok(*boxed
+                        .downcast::<T>()
+                        .expect("Impossible! The type cannot be different."))
+                } else {
+                    Err(other)
+                }
+            },
+        }
+    }
+}
+
+impl<T: Lifecycle + Debug + 'static> ComponentManager for ComponentWrapper<T> {
+    fn debug(&self) -> String {
+        format!("{:?}", self.component)
+    }
+
+    fn merge(&mut self, other: Box<ComponentManager>) -> Option<Box<ComponentManager>> {
+        match (self.try_cast)(other) {
+            // The components are same
+            Ok(other) => {
+                // Use the state/status from the older component
+                self.component.reuse_status(other.status());
+                // Update the state values to the newer updated one
+                self.component.refresh_state();
+
+                // Invoke the lifecycle event of updated.
+                self.component.updated(other.props());
+                None
+            }
+            Err(manager) => Some(manager),
+        }
+    }
+
+    fn render(&self) -> KeyedVNodes {
+        self.component.render()
+    }
+}
+
+trait Downcast: Any {
+    fn into_any(self: Box<Self>) -> Box<Any>;
+}
+
+impl<T: Any> Downcast for T {
+    fn into_any(self: Box<Self>) -> Box<Any> {
+        self
+    }
+}
