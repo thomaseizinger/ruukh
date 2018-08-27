8fe0751ebb9b424e1eb6a0e5c277c703639ee23b
Sharad Chand <sharad.d.chand@gmail.com>
2018-08-27 17:20:20 +0545

    Tore the trait into three, as type inference on RCTX make it difficult accessing remove and node methods

--- dom_ruukh/src/dom.rs
+++ dom_ruukh/src/dom.rs
@@ -1,7 +1,10 @@
+use component::RenderableComponent;
 use wasm_bindgen::prelude::JsValue;
+use web_api::Node;
+use Shared;
 
 /// Trait to patch the DOM to reflect the VDOM structure.
-pub trait DOMPatch
+pub trait DOMPatch<RCTX: RenderableComponent>
 where
     Self: Sized,
 {
@@ -9,7 +12,12 @@ where
     type Node;
 
     /// Walks through the VDOM till an uninitialized or a dirty Component is found and renders it recursively.
-    fn render_walk(&mut self, parent: Self::Node, next: Option<Self::Node>) -> Result<(), JsValue>;
+    fn render_walk(
+        &mut self,
+        parent: Self::Node,
+        next: Option<Self::Node>,
+        render_ctx: Shared<RCTX>,
+    ) -> Result<(), JsValue>;
 
     /// Patch the DOM by diffing the VDOM `Self` with Older VDOM.
     fn patch(
@@ -17,11 +25,20 @@ where
         old: Option<Self>,
         parent: Self::Node,
         next: Option<Self::Node>,
+        render_ctx: Shared<RCTX>,
     ) -> Result<(), JsValue>;
+}
+
+/// Trait to remove the VNode from the DOM.
+pub trait DOMRemove {
+    type Node;
 
     /// Remove the VDOM from the actual DOM.
     fn remove(self, parent: Self::Node) -> Result<(), JsValue>;
+}
 
+/// Glean out the info from the DOM attached VDOM.
+pub trait DOMInfo {
     /// The node value of the DOM attached VDOM.
-    fn node(&self) -> Option<Self::Node>;
+    fn node(&self) -> Option<Node>;
 }
