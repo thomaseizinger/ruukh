32f8d21ebc0bc9d73701b5aff36765506df58261
Sharad Chand <sharad.d.chand@gmail.com>
2018-09-26 07:56:27 +0545

    A simple component example

---
+++ examples/component/src/lib.rs
@@ -0,0 +1,57 @@
+#![feature(proc_macro_gen, proc_macro_non_items, decl_macro)]
+
+use ruukh::prelude::*;
+use ruukh::web_api::*;
+use wasm_bindgen::prelude::*;
+
+#[component]
+#[derive(Lifecycle)]
+struct MainApp {
+    #[state]
+    toggle: bool,
+}
+
+impl Render for MainApp {
+    fn render(&self) -> Markup<Self> {
+        html! {
+            <div style={self.style()}>{ if self.toggle { "On" } else { "Off" } }</div>
+            <Button @click={Self::toggle}></Button>
+        }
+    }
+}
+
+impl MainApp {
+    fn style(&self) -> &'static str {
+        if self.toggle {
+            "width: 100px; height: 100px; background-color: red;"
+        } else {
+            "width: 100px; height: 100px; background-color: yellow;"
+        }
+    }
+
+    fn toggle(&self, _: Event) {
+        self.set_state(|state| {
+            state.toggle = !state.toggle;
+        });
+    }
+}
+
+#[component]
+#[derive(Lifecycle)]
+#[events(
+    fn click(&self, event: Event);
+)]
+struct Button;
+
+impl Render for Button {
+    fn render(&self) -> Markup<Self> {
+        html! {
+            <button @click={Self::click}>Toggle</button>
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn run() -> ReactiveApp {
+    App::<MainApp>::new().mount("app")
+}
